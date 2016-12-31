c63771f44e293893e27df2a6808efd4c6ad4f81d
Nicholas Rohrer <nrohrer2@gmail.com>
2016-12-30 20:19:46 -0600

    added 10 wells to display streamer information on first page

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ $(document).ready();
 $.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
   console.log(data);
 });
+
+$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/summit1g?callback=?', function(data) {
+  console.log(data);
+});
