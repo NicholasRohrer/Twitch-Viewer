2992b6800cb1a373fcd6ce266b11467dfc623d2a
Nicholas Rohrer <nrohrer2@gmail.com>
2017-01-01 23:15:22 -0600

    adding number of viewers of test streamer from API call

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ $(document).ready();
 var $streamer1 = $('#streamer1');
 var $status1 = $('#status1');
 var $game1 = $('#game1');
+var $viewers1 = $('#viewers1');
 
 //$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
  //console.log(data);
@@ -19,6 +20,7 @@ function getStreamerInfo() {
 	  	$streamer1.html(data.stream['channel'].display_name);
 	  	$status1.html('Online');
 	  	$game1.html(data.stream['game']);
+	  	$viewers1.html(data.stream['viewers']);
 	  } 
 
 	  else if (data.stream == 'null') {
