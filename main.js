201873066938cdf520ae19aaa8a34aacac43c450
Nicholas Rohrer <nrohrer2@gmail.com>
2017-01-01 22:58:49 -0600

    able to add test streamer name from api call

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
 $(document).ready();
 
-$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
-  console.log(data);
-});
+//cache the DOM
+var $streamer1 = $('#streamer1');
 
-$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/summit1g?callback=?', function(data) {
-  console.log(data);
-});
+//$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
+ //console.log(data);
+//});
+
+function getStreamerInfo() {
+
+	$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/summit1g?callback=?', function(data) {
+	  console.log(data);
+
+	  if (data.stream != 'null') {
+	  	console.log(data.stream['channel'].display_name);
+	  	$streamer1.html(data.stream['channel'].display_name);
+	  } 
+	  	
+	});
+}
+
+getStreamerInfo();
