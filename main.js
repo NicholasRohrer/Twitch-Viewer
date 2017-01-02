eb3df5ba6cf6f3fa9249f8b0f474a7297bee90a2
Nicholas Rohrer <nrohrer2@gmail.com>
2017-01-01 23:09:43 -0600

    adding current game of test streamer from API call

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ $(document).ready();
 //cache the DOM
 var $streamer1 = $('#streamer1');
 var $status1 = $('#status1');
+var $game1 = $('#game1');
 
 //$.getJSON('https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?', function(data) {
  //console.log(data);
@@ -17,6 +18,7 @@ function getStreamerInfo() {
 	  	console.log(data.stream['channel'].display_name);
 	  	$streamer1.html(data.stream['channel'].display_name);
 	  	$status1.html('Online');
+	  	$game1.html(data.stream['game']);
 	  } 
 
 	  else if (data.stream == 'null') {
