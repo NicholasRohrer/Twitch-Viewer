799eda6c4d6f9ade6a90660585dcb6980c06a07c
NICHOLAS ROHRER <nrohrer2@gmail.com>
2017-01-02 22:42:00 -0600

    adding additional streamers to array

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ $(document).ready();
 //cache the DOM
 
 // list of currently popular and interesting Twitch Streamers
-var streamers = ['summit1g', 'TimTheTatman', 'freecodecamp', 'Doublelift', 'shroud', 'Ninja'];
+var streamers = ['summit1g', 'TimTheTatman', 'freecodecamp', 'Doublelift', 'shroud', 'Ninja', 'Arconyx', 'teamTALIMA', 'CohhCarnage'];
 
 function getStreamerInfo() {
 
